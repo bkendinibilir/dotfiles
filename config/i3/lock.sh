86ffe90f5a668dca355cafa1b66b3173d3c15a26
Benjamin Kendinibilir <bkendinibilir@seibert-media.net>
2018-05-28 12:18:43 +0200

    fix wrong regex for getting screen resolution

--- config/i3/lock.sh
+++ config/i3/lock.sh
@@ -4,8 +4,8 @@ TMPBG=$HOME/screenshot.png
 LOCK=$HOME/.config/i3/lock.png
 
 RES_ALL=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
-RES_eDP1x=$(xrandr | grep 'eDP1' | sed -E 's/eDP1 connected primary ([0-9]+)x([0-9]+).*/\1/')
-RES_eDP1y=$(xrandr | grep 'eDP1' | sed -E 's/eDP1 connected primary ([0-9]+)x([0-9]+).*/\2/')
+RES_eDP1x=$(xrandr | grep 'eDP1' | sed -E 's/eDP1 connected[^0-9]*([0-9]+)x([0-9]+).*/\1/')
+RES_eDP1y=$(xrandr | grep 'eDP1' | sed -E 's/eDP1 connected[^0-9]*([0-9]+)x([0-9]+).*/\2/')
 
 if xrandr -q | grep "DP2 disconnected"; then
   ffmpeg -f x11grab -video_size $RES_ALL -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=8:4,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
